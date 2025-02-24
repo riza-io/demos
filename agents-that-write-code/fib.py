af08a5d636d0724c1b1d79338ec8f4d003e52864
Kyle Gray <kyle@riza.io>
2025-02-24 14:04:56 -0800

    Remove latest demo

--- agents-that-write-code/fib.py
+++
@@ -1,9 +0,0 @@
-import codegen
-
-
-requirements = """
-calculate 52! (52 factorial)
-"""
-
-
-codegen.write_review_and_run_code(requirements)
--- agents-that-write-code/to_json.py
+++
@@ -1,8 +0,0 @@
-import codegen
-
-
-requirements = """
-
-"""
-
-codegen.write_review_and_run_code(requirements, input_filename="data.html")
