5e1b29c560546014190d1fd43510bed463ba3149
David Lee <david@riza.io>
2025-05-06 16:29:55 -0700

    support DEV_DOMAINS

--- marketing-demo/src/cors.ts
+++ marketing-demo/src/cors.ts
@@ -1,5 +1,9 @@
 const ALLOWED_ORIGINS = ["https://riza.io"];
 
+if (process.env.DEV_DOMAINS) {
+  ALLOWED_ORIGINS.push(...process.env.DEV_DOMAINS.split(","));
+}
+
 // These allow Riza's marketing website to make cross-origin requests to this backend
 export function getCORSHeaders(request: Request): HeadersInit {
   const origin = request.headers.get("origin") || "";
